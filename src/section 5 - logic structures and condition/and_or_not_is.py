730f936397a239afd397a8aa0473520407ff7949
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2021-11-25 14:22:57 -0300

    section 5 ok

---
+++ src/section 5 - logic structures and condition/and_or_not_is.py
@@ -0,0 +1,25 @@
+# Estruturas lógicas
+# and_or_not_is
+
+ativo = False
+logado = False
+
+# if (ativo and logado):
+#     print("Bem vindo usuário!")
+# else:
+#     print("Você precisa ativar sua conta!")
+
+# if (ativo or logado):
+#     print("Bem vindo usuário!")
+# else:
+#     print("Você precisa ativar sua conta!")
+
+# if not ativo:
+#     print("Você precisa ativar sua conta, cheque seu email!")
+# else:
+#     print("Bem vindo")
+
+if ativo is False:
+    print("Você precisa ativar sua conta!")
+else:
+    print("Bem-vindo usuário")
