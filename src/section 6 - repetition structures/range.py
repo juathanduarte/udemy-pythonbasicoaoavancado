9fd28aaeb97abd80b552bd3bd9be7607b3bdfaee
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-01-07 14:16:28 -0300

    range

---
+++ src/section 6 - repetition structures/range.py
@@ -0,0 +1,22 @@
+# Ranges são utilizados para gerar sequência númericas de maneira especificada.
+# range(valor_de_inicio, valor_de_fim, valor_de_passo)
+
+# Forma 1
+
+for num in range(11):
+    print(num)
+
+# Forma 2
+
+for num in range(1, 11):
+    print(num)
+
+# Forma 3    
+
+for num in range(1, 10, 2):
+    print(num)
+
+# Forma 5
+
+for num in range(10, 0, -1):
+    print(num)
