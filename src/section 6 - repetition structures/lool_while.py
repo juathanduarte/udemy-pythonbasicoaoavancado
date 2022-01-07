1a978ab343f3ba65f73464242b4ceeda6e90611b
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-01-07 14:25:45 -0300

    while

---
+++ src/section 6 - repetition structures/lool_while.py
@@ -0,0 +1,17 @@
+# while expression:
+# //executes the code block while the condition is true
+
+# Exemplo 1
+
+# num = 1
+
+# while num < 10:
+#     print(num)
+#     num += 1
+
+# Exemplo 2
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Você gostaria de sair? [sim/não]')
