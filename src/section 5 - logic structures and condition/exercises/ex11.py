df6cf5fea2f9e05669df3fbe16f2c6c1b82a69d6
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-01-07 14:09:06 -0300

    loop for

--- src/section 5 - logic structures and condition/exercises/ex11.py
+++ src/section 5 - logic structures and condition/exercises/ex11.py
@@ -1,3 +1,5 @@
 # Escreva um programa que leia um número inteiro maior do que zero e devolva na tela a soma de todos seus algarismos. Se o número lido não for maior do que zero, o programa terminará com a mensagem "Número inválido"
 
+# fiquei com preguiça
+
 number = int(input("Digite um número inteiro: "))
--- src/section 6 - repetition structures/loop_for.py
+++ src/section 6 - repetition structures/loop_for.py
@@ -1,5 +1,57 @@
-# Loop for
-# Estrutura de repetição
+# # # Loop for
+# # # Estrutura de repetição
 
-# for item in interavel:
-#       EXECUTA O LOOP
+# # # for item in interavel:
+# # #       EXECUTA O LOOP
+
+# # nome = 'Geek University'
+# # lista = [1, 3, 5, 7, 9]
+# # numeros = range(1, 10)
+
+# # # Exemplo 1
+
+# # for letra in nome:
+# #     print(letra)
+
+# # # Exemplo 2
+
+# # for numero in lista:
+# #     print(numero)
+
+# # # Exemplo 3
+
+# # for numero in range(1, 10):
+# #     print(numero)
+
+# # # Exemplo 4
+
+# # for i, letra in enumerate(nome):
+# #     print(nome[i])
+
+# # # Exemplo 5
+
+# # for i, letra in enumerate(nome):
+# #     print(letra)
+
+# # # Exemplo 6
+
+# # for _, letra in enumerate(nome):
+# #     print(letra)
+
+# # # Exemplo 7
+
+# # for valor in enumerate(nome):
+# #     print(valor)
+
+# qtd = int(input('Quantas vezes esse loop deve rodar?'))
+# soma = 0
+
+# for n in range(1, qtd + 1):
+#     num = int(input(f'Informe o {n}/{qtd} valor:'))
+#     soma += num
+# print(f'A soma dos valores foi {soma}')
+
+nome = 'Geek University'
+
+for letra in nome:
+    print(letra, end='')
